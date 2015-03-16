612313f4af0f7cd75a128959daf9e739bd66425a
Jay@linux <shizhongxian@126.com>
2015-03-16 16:10:21 +0800

    add log module

--- ch_en_xlsxcomp.py
+++ ch_en_xlsxcomp.py
@@ -4,16 +4,66 @@
 import os
 import os.path
 import string
+import logging
 from openpyxl import load_workbook
 import openpyxl.cell as ce
 
+LOG_FILENAME = 'log.txt'
+FORMAT  = '%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s'
+DATEFMT = '%a,%d %b %Y %H:%M:%S' 
+logging.basicConfig(format = FORMAT,datefmt = DATEFMT,filename = LOG_FILENAME,filemode = 'w',level=logging.DEBUG)
 
+    
+def value_change(value):
+    """
+    对单元格的值进行转化
+    """
+    result = ""
+    if type(value) == unicode:
+        result = string.strip(value)
+    else:
+        result = str(value)
+        result = string.strip(result)
+    return result
+
+def del_none_row(l):
+    """
+    删除二维数组中空白的行
+    """
+    empty_row_num = []
+    for row_num in range(len(l)):
+        count = 0
+        for value in l[row_num]:
+            if value == None or value_change(value) == "":
+                count += 1
+        if count < len(l[row_num]):
+            empty_row_num.append(row_num)
+    result = []
+    for row_num in empty_row_num:
+        result.append(l[row_num])
+    return result
+
+def get_dims(l):
+    """
+    
+    """
+    row_size = len(l)
+    col_size = len(l[0])
+    for row in l:
+        if col_size == len(row):
+            pass
+        else:
+            return [-1,-1]
+    return [row_size,col_size]
+        
+            
 class XlsxTableHeader:
     """
     
     """
     def __init__(self,filename):
         try:
+            self.filename = filename
             self.wb = load_workbook(filename)
             #self.sname = filename[-5]+"_tmp.xlsx"
             self.ws = self.wb.active
@@ -31,10 +81,14 @@ class XlsxTableHeader:
             #默认从第一列开始
             self.theader_start_col_num = 1
             self.start = True
-            print "表头区域是",self.theader_range
+            msg = "表头区域：" + str(self.theader_range)
+            #print "表头区域是",self.theader_range
+            logging.info(msg)
             
         except:
             print "初始化",filename,"失败"
+            msg = "初始化" + filename + "失败"
+            logging.error(msg)
             self.start = False
     def get_theader_content(self):
         """
@@ -115,6 +169,8 @@ class XlsxTableHeader:
         max_col,max_row = ce.column_index_from_string(self.ws[cell_end].column),self.ws[cell_end].row
         self.rownum_range = [min_row,max_row]
         self.colnum_range = [ce.get_column_letter(min_col),ce.get_column_letter(max_col)]
+        #表头区域大小
+        self.theader_area_size = [max_row-min_row+1,max_col-min_col+1]
         #pos = self.theader_range.find(":")
         #if len(self.theader_range[pos+1:]) == 2:
         #    self.colnum_range = [self.theader_range[0],self.theader_range[3]]
@@ -236,20 +292,16 @@ class XlsxTableHeader:
             merged_cells = self.get_all_cells(merged_area)
             scell_num = merged_cells[0]
             #不用把单元格值转化为str 然后在进行string.strip() ,可以直接对单元格值进行string.strip()
-            if ws[scell_num].value != None and string.strip(ws[scell_num].value) !="":
+            if ws[scell_num].value != None and value_change(ws[scell_num].value) != "":
                 pass
             else:
                 for idx in range(1,len(merged_cells)):
                     cell_num = merged_cells[idx]
-                    if ws[cell_num].value != None and string.strip(ws[cell_num].value) != "":
+                    #if ws[cell_num].value != None and string.strip(ws[cell_num].value) != "":
+                    if ws[cell_num].value != None and value_change(ws[cell_num].value) != "":
                         ws[scell_num].value = ws[cell_num].value
                         ws[cell_num].value = None
-    
-    def first_cell_value(self,ws,merged_areas_list):
-        """
-        合并区域中的值置于第一个单元格
-        """
-        pass
+
   
     def merge_range(self,ws,can_merged_range):
         """
@@ -275,12 +327,14 @@ class XlsxTableHeader:
                             and ws[all_cells[0]].style.border.bottom.style == None  \
                             and ws[all_cells[-1]].style.border.top.style == None:
                             #print "合并前",ws[all_cells[0]].value,ws[all_cells[-1]].value
-                            ws[all_cells[0]].value = ws[all_cells[0]].value  + " " + ws[all_cells[-1]].value
+                            ws[all_cells[0]].value = value_change(ws[all_cells[0]].value)  + " " + value_change(ws[all_cells[-1]].value)
                             ws[all_cells[-1]].value = None
                             #print all_cells,"单元格值合并成功"
                             #print "合并后",ws[all_cells[0]].value,ws[all_cells[-1]].value
                         else:
-                            print all_cells,"单元格值不能合并"
+                            #print all_cells,"单元格值不能合并"
+                            msg = self.filename + str(all_cells) + "单元格值不能合并"
+                            logging.info(msg)
                     #超过两行，判断中间行是否包含边框
                     else:
                         if ws[all_cells[0]].style.border.top.style and ws[all_cells[-1]].style.border.bottom.style:
@@ -289,15 +343,19 @@ class XlsxTableHeader:
                                 #如果有一个单元格存在上下边框线，则不能合并
                                 if ws[all_cells[idx]].style.border.top.style or ws[all_cells[idx]].style.border.bottom.style :
                                     merge_flag = 0;
-                                    print all_cells,"单元格值不能合并，有边框"
+                                    #print all_cells,"单元格值不能合并，有边框"
+                                    msg = self.filename + str(all_cells) + "单元格值不能合并，有边框"
+                                    logging.info(msg)
                                     break
                             if merge_flag ==1 :
                                 for idx in range(1,len(all_cells))[::-1]:
-                                    ws[all_cells[idx-1]].value = ws[all_cells[idx-1]].value + " " + ws[all_cells[idx]].value 
+                                    ws[all_cells[idx-1]].value = value_change(ws[all_cells[idx-1]].value) + " " + value_change(ws[all_cells[idx]].value) 
                                     ws[all_cells[idx]].value  = ""
                                 #print all_cells,"单元格值合并成功！"   
                 else:
-                    print "行数少于两行 error"
+                    #print "行数少于两行 error"
+                    msg = self.filename + "行数少于两行"
+                    logging.info(msg)
     
     def get_content(self):
         """
@@ -317,7 +375,7 @@ def ch_en_xlsxcomp():
     根据给定的中英文对应表格，完成中英文表头的对应输出
     """
     fout = open("TableHeaderContent.txt",'a')
-    flog = open("log.txt","w")
+    #flog = open("log.txt","w")
     conf_file = open("conf.data","r")
     conf_lines = conf_file.readlines()
     ch_dir = ""
@@ -334,56 +392,69 @@ def ch_en_xlsxcomp():
             filename_list.append(filename)
     
     #
+    #for i in range(1):
     for filename in filename_list:
+        #filename = "A0111c.xlsx"
         ch_xlsx_name = ch_dir + filename
         en_xlsx_name = en_dir + filename
-        print "当前处理文件：",filename
-        flog.write(filename+"\n")
+        print "当前处理文件号：",filename
+        msg = "当前处理文件号:" + filename
+        logging.info(msg)
+        #flog.write(filename+"\n")
         ch_xl = XlsxTableHeader(ch_xlsx_name)
         en_xl = XlsxTableHeader(en_xlsx_name)
-        fout.write(filename+"----------\n")
+        
         if ch_xl.start and en_xl.start:
             ch_content = ch_xl.get_theader_content()
             en_content = en_xl.get_theader_content()
-            if len(ch_content) == len(en_content):
+            ch_content = del_none_row(ch_content)
+            en_content = del_none_row(en_content)
+            if get_dims(ch_content) != [-1,-1]  and get_dims(en_content) != [-1,-1] and get_dims(ch_content) == get_dims(en_content):
+                fout.write(filename+"----------\n")
                 for idx_row in range(len(ch_content)):
                     for idx_col in range(len(ch_content[idx_row])):
                         if (ch_content[idx_row][idx_col] != None and ch_content[idx_row][idx_col] != "") \
                             or (en_content[idx_row][idx_col] != None and en_content[idx_row][idx_col] != ""):
-                            try:
-                                ch_ = ch_content[idx_row][idx_col]
-                                en_ = en_content[idx_row][idx_col]
-                                if type(ch_) == unicode:
-                                    ch_ = ch_.replace("\n"," ")
-                                if type(en_) == unicode:
-                                    en_ = en_.replace("\n"," ")
-                                if ch_ == None:
-                                    ch_ = u"None"
-                                if en_ == None:
-                                    en_ = u"None"
+                            #try:
+                            ch_ = ch_content[idx_row][idx_col]
+                            en_ = en_content[idx_row][idx_col]
+                            if type(ch_) == unicode:
+                                ch_ = ch_.replace("\n"," ")
+                            if type(en_) == unicode:
+                                en_ = en_.replace("\n"," ")
+                            if ch_ == None:
+                                ch_ = u"None"
+                            if en_ == None:
+                                en_ = u"None"
                                 #print ch_,"||",en_
-                                fout.write(ch_.encode('UTF-8'))
-                                fout.write("||")
-                                fout.write(en_.encode('UTF-8'))
-                                fout.write("\n")
-                            except:
-                                fout.write(ch_.encode('UTF-8'))
-                                fout.write("value error \n")
+                                #fout.write(ch_.encode('UTF-8'))
+                            fout.write(value_change(ch_).encode('UTF-8'))
+                            fout.write("||")
+                            #fout.write(en_.encode('UTF-8'))
+                            fout.write(value_change(en_).encode('UTF-8'))
+                            fout.write("\n")
+                            #except:
+                                #fout.write(ch_.encode('UTF-8'))
+                                #fout.write("value error \n")
                         else:
                             pass    
             else:
-                fout.write("表头合并后格式不一样\n")                    
+                #fout.write("表头区域大小不一致\n")
+                msg = filename + "表头区域不一致"
+                logging.info(msg)                    
         else:
             fout.write("文件初始化没有完成\n")
+            msg = filename + "文件初始化没有完成"
+            logging.info(msg)
             print "文件",filename,"有错误，请检查"
     fout.close()
-    flog.close()
+    #flog.close()
     print "Compare End!"
 
 
 if __name__ == "__main__":
     ch_en_xlsxcomp()
-    #ch_xl = XlsxTableHeader("./ch_xlsx/A0101a.xlsx")
+    #ch_xl = XlsxTableHeader("./ch_xlsx/B0903a.xlsx")
     #if ch_xl.start:
     #    ch_xl.get_theader_content()
     #    for line in ch_xl.content:
